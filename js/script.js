ead7de69cb76911ce1223e3ed019a2ab9f1f10a0
UmmerZ <ummer.zaman88@gmail.com>
2020-09-05 12:46:17 -0600

    contact form updated

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+const button = document.getElementById("button");
+
+button.addEventListener("click", function () {
+  window.getElementById("button");
+  const form = document.createElement("li");
+  form.innerHTML = name.value;
+  if (form.contains("fuck")) {
+    alert("bad language");
+  }
+});
