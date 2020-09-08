bae5c80149393f235c0a9542d056ccbab0102b9d
UmmerZ <ummer.zaman88@gmail.com>
2020-09-08 01:47:42 -0600

    added mailto feature triggered with js after validation.

---
+++ Readme.md
@@ -0,0 +1,11 @@
+trello link : https://trello.com/b/2sZCgKW9/milestone-project-1
+Borrowed code for js from:
+https://www.rhyous.com/2015/05/27/how-to-populate-mailto-link-with-a-form-action-using-javascript/
+https://www.youtube.com/watch?v=rsd4FNGTRBw
+https://stackoverflow.com/questions/21028939/mailto-using-javascript
+
+
+
+
+I have 2 errors in index.html which were because of p tags inside a div tag which I researacheda 
+a lot but seems to  be no fix. I had no fix around that as I had to use it beacuse I had to use read me buttons
--- contact/contact.js
+++ contact/contact.js
@@ -1,21 +1,54 @@
 const name = document.getElementById("name");
 const email = document.getElementById("email");
 const subject = document.getElementById("subject");
-const message = document.getElementById("message");
+const message = document.getElementById("textArea");
 const form = document.getElementById("form");
+const number = document.getElementById("number");
+let swear = false;
 
 const swearWords = ["feldercarb", "frack", "skinjob", "vulgacarb"];
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
+  
 
   swearCheck(e);
-  console.log();
+  
 });
-function swearCheck() {
-  const messageValue = message.value();
+
+function swearCheck(e) {
+  swear = false;
+  document.getElementById('textArea');
+
+  const messageValue = message.value;
+
   for (let i = 0; i < swearWords.length; i++) {
-    if (swearWords[i] === messageValue);
-    alert("word found");
+    const word = swearWords[i];
+
+    if (messageValue.includes(word)) {
+
+      const errorMessage = document.createElement('LI')
+      errorMessage.textContent = `We found a word "  ${word} " in the Email. Please avoid profanity as we belive in professionalism`;
+      errorList.appendChild(errorMessage);
+      swear = true;
+    }
   }
+
+  if (swear !== true) {
+  
+    const body = document.getElementById("textArea").value;
+    const Fname = document.getElementById("name").value;
+    const mySubject = document.getElementById("subject").value;
+    const myNumber = document.getElementById("number").value;
+    const mail = ("Full Name: " + Fname  + "%0D%0A" + "Subject: " + mySubject 
+                 +  "%0D%0A" + "Phone number: " + myNumber  + "%0D%0A" + " Message: " + body);
+      window.location.href = 'mailto:write2ummer@yahoo.com?body=' + mail;
+    
+  }
+  else {
+    e.preventDefault();
+  }
+
+
 }
+    
